e6e2234b79bfdb32633655f675467fc1c395ed2d
Luis Rodriguez <luis01.ro@gmail.com>
2019-09-14 21:53:54 -0500

    "Added conversion of temp from K to F"

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const openWeatherKey = '527be69af9e0687cce86953198b12581'
 const mapsKey = 'AIzaSyDZO7BmldQ7TPGohXTa-PxNaoF81D7Ofuw'
 function startApp() {
   watchForm()
+  $('#result').hide();
 
 
 }
@@ -25,13 +26,14 @@ function getWeather(location) {
 }
 
 function displayWeather(data) {
+  var convToF = (parseInt((`${data.main.temp}`) - 273.15)*(9/5)+32);
   $('#result-weather').empty();
   $('#js-error-message').empty();
   $('#result-weather').append(
     `<h3>${data.name}</h3>
     <li> Humidity: ${data.main.humidity} %</li>
-    <li> Currently the day has a ${data.weather[0].description}</li>
-    <li> The current temperature is: ${data.main.temp} K</li>`
+    <li> Currently the day has  ${data.weather[0].description}</li>
+    <li> The current temperature is: ${convToF} F</li>`
 
   );
 
@@ -45,21 +47,22 @@ var service;
 var infowindow;
 
 function initMap(location) {
-  var sydney = new google.maps.LatLng(-33.867, 151.195);
+  var unitedStates = new google.maps.LatLng(37.0902, 95.7129);
 
   infowindow = new google.maps.InfoWindow();
 
   map = new google.maps.Map(
 
-    document.getElementById('map-area'), { center: sydney, zoom: 15 });
+    document.getElementById('map-area'), { center: unitedStates, zoom: 10 });
 
   var request = {
     query: location,
-    fields: ['name', 'geometry'],
-    type: 'park',
+    fields: ['name', 'geometry','placeId'],
+    type: 'park'
   };
 
   service = new google.maps.places.PlacesService(map);
+  
 
   service.textSearch(request, function (results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -69,9 +72,13 @@ function initMap(location) {
 
       map.setCenter(results[0].geometry.location);
     }
+    
+  
   });
   $('#result').show();
+  
   console.log('initMap Loaded')
+  
 
 }
 
@@ -86,6 +93,9 @@ function createMarker(place) {
     infowindow.open(map, this);
   });
 }
+
+
+
 
 
 function watchForm() {
